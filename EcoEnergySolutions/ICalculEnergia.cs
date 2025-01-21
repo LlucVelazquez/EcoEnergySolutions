d3e4dde6d5de0dcd5ba0190d8499bd7c30477d24
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-01-21 10:35:58 +0100

    Created Interface ICalculEnergia

---
+++ EcoEnergySolutions/ICalculEnergia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEnergySolutions
+{
+    public interface ICalculEnergia
+    {
+        void ConfigurarParametres(double valor);
+        double CalcularEnergia();
+        void MostrarInforme();
+
+    }
+}
