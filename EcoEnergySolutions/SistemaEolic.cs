cdd79f436d9c889e5f48bd6e42b4f07d0fd9d275
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-01-21 10:44:10 +0100

    Created SubClasses

---
+++ EcoEnergySolutions/SistemaEolic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEnergySolutions
+{
+    public class SistemaEolic : SistemaEnergia
+    {
+    }
+}
