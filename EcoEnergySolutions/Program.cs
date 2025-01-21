74b1fd7a07f76b3b9b05fe5f926b7448243b6b4a
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-01-21 09:13:58 +0100

    Add project files.

---
+++ EcoEnergySolutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoEnergySolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
